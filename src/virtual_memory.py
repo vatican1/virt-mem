4431433199a292d945827e79d5db4321d2661474
vatican1 <33574109+vatican1@users.noreply.github.com>
2020-09-29 23:26:40 +0300

    переделал OPT

--- src/virtual_memory.py
+++ src/virtual_memory.py
@@ -55,38 +55,42 @@ def lru_algo(M, list_of_pages):
     return quantity_of_loads
 
 
-def find_min(use_pages):
-    min = use_pages[0][1]
-    min_arr = use_pages[0]
-    for i in use_pages:
-        if i[1] < min:
-            min_arr = i
-    return min_arr
+def find_min(use_pages):  # [a,b,c,d,e][1,1,1,1,1]
+    min_index_of_el = 0
+    min_el = [use_pages[0][0], use_pages[1][0]]
+    for i in enumerate(use_pages[1]):
+        if i[1] < min_el[1]:
+            min_el = [use_pages[0][i[0]], use_pages[1][i[0]]]
+            min_index_of_el = i[0]
+    return min_index_of_el
 
 
 def opt_algo(M, list_of_pages):
-    list_of_pages_weight = []  # Массив, где хранится, количество раз, когда страница встречается в последовательности
+    list_of_pages_weight = [[], []]
+    # Массив, где хранится, количество раз, когда страница встречается в последовательности
     for i in enumerate(list_of_pages):
-        list_of_pages_weight.append([i[1], list_of_pages[i[0]:].count(i[1])])
+        list_of_pages_weight[0].append(i[1])
+        list_of_pages_weight[1].append(list_of_pages[i[0]:].count(i[1]))
     # for i in list_of_pages:
     #     list_of_pages_weight.append([i, list_of_pages.count(i)])
 
-
     quantity_of_loads = 0
-    use_pages = [list_of_pages_weight[0]]
+    use_pages = [[list_of_pages_weight[0][0]], [list_of_pages_weight[1][0]]]
     k = 1  # k - показывает количество элементов взятых из списка страниц и загруженных в память,
     # k может быть больше M из-за повторяющихся страниц
-    while len(use_pages) != M:
-        if use_pages[0].count(list_of_pages_weight[k][0]) == 0:
-            use_pages.append(list_of_pages_weight[k])
+    while len(use_pages[0]) != M:
+        if use_pages[0].count(list_of_pages_weight[0][k]) == 0:
+            use_pages[0].append(list_of_pages_weight[0][k])
+            use_pages[1].append(list_of_pages_weight[1][k])
         k += 1
 
-    for i in range(len(list_of_pages_weight) - k):  # Запускаем цикл на все элементы после k-ого
+    for i in range(len(list_of_pages_weight[0]) - k):  # Запускаем цикл на все элементы после k-ого
         # print(list_of_pages_weight[i + k])
-        if use_pages.count(list_of_pages_weight[i + k]) == 0:
-            min = find_min(use_pages)  # тут находим элемент, встречающийся минимальное количество раз
-            use_pages = use_pages[0:use_pages.index(min)] + use_pages[use_pages.index(min) + 1:] + [
-                list_of_pages_weight[i + k]]  # ставим вместо удалённой страницы
+        if use_pages[0].count(list_of_pages_weight[0][i + k]) == 0:
+            index_min = find_min(use_pages)  # тут находим индекс элемента, встречающегося минимальное количество раз
+            use_pages[0] = use_pages[0][0:index_min] + use_pages[0][index_min + 1:] + [list_of_pages_weight[0][i + k]]
+            use_pages[1] = use_pages[1][0:index_min] + use_pages[1][index_min + 1:] + [list_of_pages_weight[1][i + k]]
+            # ставим вместо удалённой страницы
             quantity_of_loads += 1
     return quantity_of_loads
 
